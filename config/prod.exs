02233f5ca3bd0bffda8a9ab5bc04097245ca7860
Suraj Kumar <ravi.suraj110@gmail.com>
2017-11-20 11:38:34 +0530

    change app name

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :bookstore, BookstoreWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: "polar-beyond-93606.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "recommendedbookstore.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
